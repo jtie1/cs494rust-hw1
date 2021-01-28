838faf609a41fe52d13131253cc6a35e6ba4ef1c
Gabe Tiemann <thetiemann01@gmail.com>
2021-01-28 13:13:44 -0600

    sad and very late... more to come

--- src/main.rs
+++ src/main.rs
@@ -6,56 +6,70 @@
 * Professor:    Jakob Eriksson
 */
 
-// use std::io::*;
-
-/*
-    Testing for:
-    - no parameters
-        - read from stdin
-    - flags
-        * -w    words
-        * -c    bytes
-                - String.len() counts in bytes https://doc.rust-lang.org/std/string/struct.String.html#method.len 
-        * -l    lines
-                TRY: counting \n characters
-        - passed with and without parameters
-    - one or multiple files passed in
-*/
+use std::io::*;
+// use std::fs::File;
 
 fn main() {
 
-    let args: Vec<String> = std::env::args().collect();
+    // track which flags are given
+    let mut byte_count = false;
+    let mut word_count = false;
+    let mut line_count = false;
+    // grab arguments
+    let args = BufReader::new(stdin());
+    // store input file
+    let mut filename: Vec<String> = Vec::new();
 
-    // pseudo code
-    // if ( &args[1] is NOT present )
-    //     standard input
-    // else 
-    //     let filename = &args[1];
-
-    let filename = &args[1];
-    println!("In file {}", filename);
-
-    let contents = std::fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n\n{}", contents);
-    println!("bytes: {}", contents.len());
-    println!("chars: {}", contents.chars().count());
-
-    let mut newLines = contents.lines();
-    // TODO: maybe use lines().collect() ?
-    // https://doc.rust-lang.org/std/iter/trait.Iterator.html
-    let mut lineSub = newLines.next();
-    // let iterbitter = contents.iter();
-    let mut count = 0;
-    let done = false;
-    for x in 0..10 {
-        // if newLines == NULL{
-        //     done = true;
-        // }
-        count += 1;
-        println!("count: {}", count);
-        println!("next: {:?}", lineSub);
-        lineSub = lineSub.next();
+    for x in args.lines(){
+        println!("{:?}", x);
     }
+    // let contents = std::fs::read_to_string(filename)
+    //     .expect("Something went wrong reading the file");
+
+    // println!("With text:\n\n{}", contents);
+    // println!("bytes: {}", contents.len());
+    // println!("chars: {}", contents.chars().count());
+
+    // let mut newLines: String = contents.lines().collect();
+    // // TODO: maybe use lines().collect() ?
+    // // https://doc.rust-lang.org/std/iter/trait.Iterator.html
+    // let mut lineSub = newLines.next();
+    // let mut count = 0;
+    // let done = false;
+    // for x in 0..10 {
+    //     count += 1;
+    //     println!("count: {}", count);
+    //     println!("next: {:?}", lineSub);
+    // }
 }
+
+
+// /******** Helper Functions ********/
+
+
+// /* Fn: print_bytes()
+//     Desc: print info for all flags */
+// fn print_bytes(){
+
+// }
+    
+
+// /* Fn: print_words()
+//     Desc: print info for all flags */
+// fn print_words(){
+
+// }
+
+
+// /* Fn: print_lines()
+//     Desc: print info for all flags */
+// fn print_lines(){
+
+// }
+
+
+// /* Fn: print_all()
+//     Desc: print info for all flags */
+// fn print_all(){
+
+// }
