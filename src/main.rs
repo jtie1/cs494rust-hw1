fa963ba5ab3118ad49d4f60dc8c8cd36e51297fb
Gabe Tiemann <thetiemann01@gmail.com>
2021-01-28 16:47:43 -0600

    kinda works

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,15 @@ fn main() -> Result<()> {
     let mut total_words = 0;
     let mut total_lines = 0;
     let mut total_bytes = 0;
+
     // track which flags are given
     let mut byte_ = false;
     let mut word_ = false;
     let mut line_ = false;
+
     // grab arguments
     let args = BufReader::new(stdin());
+
     // store input file
     let mut filenames: Vec<String> = Vec::new();
 
@@ -75,31 +78,31 @@ fn main() -> Result<()> {
             // print results
             // if all flags are given or no flags are given
             if (byte_ && word_ && line_) || (!byte_ && !word_ && !line_){
-                println!("  {}  {}  {}  {}", line_count, word_count, contents.len(), filenames[i]);
+                println!("\t{}\t{}\t{}\t{}", line_count, word_count, contents.len(), filenames[i]);
             }
             // -wl or lw
             else if !byte_ && word_ && line_{
-                println!("  {}  {}  {}", word_count, line_count, filenames[i]);
+                println!("\t{}\t{}\t{}", word_count, line_count, filenames[i]);
             }
             // -cl or lc
             else if byte_ && !word_ && line_{
-                println!("  {}  {}  {}", line_count, contents.len(), filenames[i]);
+                println!("\t{}\t{}\t{}", line_count, contents.len(), filenames[i]);
             }
             // -cw or wc
             else if byte_ && word_ && !line_{
-                println!("  {}  {}  {}", word_count, contents.len(), filenames[i]);
+                println!("\t{}\t{}\t{}", word_count, contents.len(), filenames[i]);
             }
             // -c
             else if byte_ && !word_ && !line_{
-                println!("  {}  {}", contents.len(), filenames[i]);
+                println!("\t{}\t{}", contents.len(), filenames[i]);
             }
             // -w
             else if !byte_ && word_ && !line_{
-                println!("  {}  {}", word_count, filenames[i]);
+                println!("\t{}\t{}", word_count, filenames[i]);
             }
             // -l
             else if !byte_ && !word_ && line_{
-                println!("  {}  {}", line_count, filenames[i]);
+                println!("\t{}\t{}", line_count, filenames[i]);
             }
 
             total_words += word_count;
@@ -108,58 +111,9 @@ fn main() -> Result<()> {
             i += 1;
         }
         if i > 1{
-            println!("  {}  {}  {}  total", total_lines, total_words, total_bytes);
+            println!("\t{}\t{}\t{}\ttotal", total_lines, total_words, total_bytes);
         }
     }
-    
-    // let contents = std::fs::read_to_string(filenames)
-    //     .expect("Something went wrong reading the file");
 
-    // println!("With text:\n\n{}", contents);
-    // println!("bytes: {}", contents.len());
-    // println!("chars: {}", contents.chars().count());
-
-    // let mut newLines: String = contents.lines().collect();
-    // // TODO: maybe use lines().collect() ?
-    // // https://doc.rust-lang.org/std/iter/trait.Iterator.html
-    // let mut lineSub = newLines.next();
-    // let mut count = 0;
-    // let done = false;
-    // for x in 0..10 {
-    //     count += 1;
-    //     println!("count: {}", count);
-    //     println!("next: {:?}", lineSub);
-    // }
     Ok(())
 }
-
-
-// /******** Helper Functions ********/
-
-
-// /* Fn: print_bytes()
-//     Desc: print info for all flags */
-// fn print_bytes(){
-
-// }
-    
-
-// /* Fn: print_words()
-//     Desc: print info for all flags */
-// fn print_words(){
-
-// }
-
-
-// /* Fn: print_lines()
-//     Desc: print info for all flags */
-// fn print_lines(){
-
-// }
-
-
-// /* Fn: print_all()
-//     Desc: print info for all flags */
-// fn print_all(){
-
-// }
